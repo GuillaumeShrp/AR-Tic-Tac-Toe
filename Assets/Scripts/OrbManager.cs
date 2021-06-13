7f0f956dca29dc1bec4172d1ae1487da36de0f0d
GuillaumeShrp <guillaume.sherpa@gmail.com>
2021-06-13 19:31:05 +0200

    Add test scene + draft orb transform

--- Assets/Scripts/OrbManager.cs
+++ Assets/Scripts/OrbManager.cs
@@ -7,24 +7,32 @@ public class OrbManager : MonoBehaviour
     public bool stopRays;
     public float maxRayDistance = 100.0f;
     public GameObject[] orbs;
+    public GameObject square;
     public LayerMask collisionLayer = 1 << 10 ;
+
+    void Start()
+    {
+        square.SetActive(false);
+    }
 
     void Update()
     {
 
 #if UNITY_EDITOR
         if (Input.GetMouseButtonDown (0)&&!stopRays) {
-            Debug.Log("okk1");
+            Debug.Log("ray casted");
             Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit, maxRayDistance, collisionLayer))
             {
-            Debug.Log("cestbon");
+            Debug.Log("orb selected");
                 for(int k = 0; k < orbs.Length; k++)
                 {
                     if (hit.transform.gameObject.name == orbs[k].name)
                     {
                         orbs[k].SetActive(false);
+                        square.SetActive(true);
+                        //if with reference to game manager to know wich object to reveal
                     }
                 }
             }
@@ -40,7 +48,7 @@ public class OrbManager : MonoBehaviour
                 {
                     if (hit.transform.gameObject == orbs[k])
                     {
-                        Destroy(orbs[k]);
+                        //TOCOPY
                     }
                 }
             }
